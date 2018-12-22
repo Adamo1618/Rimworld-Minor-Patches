c533b10b869cb80a114bee3972b0ba99c119ef0f
Adamo1618 <adamekeroth@hotmail.com>
2018-12-22 22:56:47 +0100

    Initial commit

---
+++ Minor Patches/Source/Variable Bleed Rate Framework/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace VariableBleedRate
+{
+    [StaticConstructorOnStartup]
+    internal static class Main
+    {
+        static Main()
+        {
+            Log.Message("VBRF :: Patching TotalBleedRate");
+            HarmonyInstance harmonyInstance = HarmonyInstance.Create("com.rate.bleed");
+            harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(HediffSet), "CalculateBleedRate")]
+    internal static class bleedRatePatch
+    {
+        public static void Postfix(ref float __result, HediffSet __instance)
+        {
+            __result *= StatExtension.GetStatValue(__instance.pawn, StatDef.Named("BleedRate"), true);
+        }
+    }
+}
